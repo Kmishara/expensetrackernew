d50f30b885b6d783d0be9271d596047de566b7eb
Kmishara <mishrashanvi7979@gmail.com>
2025-06-03 08:51:32 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomeComponent from '/home/HomeComponent';
+import HomeComponent from './home/HomeComponent';
 const App = () => {
  
   return (
